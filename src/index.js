1430437cc8e42fa712cde3d84a09cf05594d0e3f
Heorhi Kartavenko <94631380+George-RB@users.noreply.github.com>
2025-03-31 14:50:33 +0000

    :add: js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello");
+function showAlert() {
+  alert("Регистрация успешна!");
+}
+
+const modal = document.getElementById("modal");
+const registerBtn = document.getElementById("learnMore");
+const closeModal = document.getElementById("closeModal");
+
+registerBtn.addEventListener("click", () => modal.classList.remove("hidden"));
+closeModal.addEventListener("click", () => modal.classList.add("hidden"));
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) modal.classList.add("hidden");
+});
